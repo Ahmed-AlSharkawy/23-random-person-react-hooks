567e975afc3a32deb586593ca3cdb19234390e30
ahmed <fmt.a.sharkawy@gmail.com>
2021-07-23 09:41:52 +0000

    app complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,91 @@
 import React, { useState, useEffect } from 'react'
-import {
-  FaEnvelopeOpen,
-  FaUser,
-  FaCalendarTimes,
-  FaMap,
-  FaPhone,
-  FaLock,
-} from 'react-icons/fa'
+import buttons from './buttons'
+
 const url = 'https://randomuser.me/api/'
 const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
 function App() {
-  return <h2>random user starter</h2>
+  const [isLoading, setIsLoading] = useState(true)
+  const [person, setPerson] = useState(null)
+  const [title, setTitle] = useState('name')
+  const [value, setValue] = useState('random person')
+
+  const getPerson = async () => {
+    setIsLoading(true)
+    const response = await fetch(url)
+    const newPerson = await response.json()
+    console.log(newPerson)
+    const {
+      phone,
+      email,
+      picture: { large: image },
+      login: { password },
+      name: { first, last },
+      dob: { age },
+      location: {
+        street: { number, name },
+      },
+    } = newPerson.results[0]
+
+    setPerson({
+      name: `${first} ${last}`,
+      image,
+      phone,
+      email,
+      street: `${number} ${name}`,
+      age,
+      password,
+    })
+
+    setTitle('name')
+    setValue(`${first} ${last}`)
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    getPerson()
+  }, [])
+
+  const handleValue = (e) => {
+    if (e.target.classList.contains('icon')) {
+      const newValue = e.target.dataset.label
+      setTitle(newValue)
+      setValue(person[newValue])
+    }
+  }
+  return (
+    <main>
+      <div className='block bcg-black'></div>
+      <div className='block'>
+        <div className='container'>
+          <img
+            src={(person && person.image) || defaultImage}
+            alt='randome user'
+            className='user-img'
+          />
+          <p className='user-title'>my {title} is</p>
+          <p className='user-value'>{value}</p>
+          <div className='values-list'>
+            {buttons.map((button) => {
+              return (
+                <button
+                  title={(person && person[button.label]) || button.label}
+                  className='icon'
+                  key={button.id}
+                  data-label={button.label}
+                  onMouseOver={(e) => handleValue(e)}
+                >
+                  {<button.icon />}
+                </button>
+              )
+            })}
+          </div>
+          <button type='button' className='btn' onClick={getPerson}>
+            {isLoading ? 'is loading...' : 'random  user'}
+          </button>
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default App
---
+++ src/buttons.js
@@ -0,0 +1,18 @@
+import {
+  FaEnvelopeOpen,
+  FaUser,
+  FaCalendarTimes,
+  FaMap,
+  FaPhone,
+  FaLock,
+} from 'react-icons/fa'
+
+const buttons = [
+  { id: 1, label: 'name', icon: FaUser },
+  { id: 2, label: 'email', icon: FaEnvelopeOpen },
+  { id: 3, label: 'age', icon: FaCalendarTimes },
+  { id: 4, label: 'street', icon: FaMap },
+  { id: 5, label: 'phone', icon: FaPhone },
+  { id: 6, label: 'password', icon: FaLock },
+]
+export default buttons
